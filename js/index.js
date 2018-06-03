02296075e04826c7a2474ad1d96bbe3e17bca7a5
coinstats <39546413+coinstats@users.noreply.github.com>
2018-06-03 02:08:30 +0200

    add chart price, move zoom, update tooltip

--- js/index.js
+++ js/index.js
@@ -4,6 +4,10 @@ var currency = 'BTC';
 var baseCurrency = 'USD';
 var zoom = '1m';
 var chart;
+var chartPriceOpen = 0;
+var chartPriceClose = 0;
+var chartPriceMin = 0;
+var chartPriceMax = 0;
 var allCurrencies = {};
 
 var currencies = [
@@ -94,12 +98,15 @@ var options = {
 			var s;
 			s = Highcharts.dateFormat('%Y-%m-%d %H:%M:%S', new Date(this.x)) + "<br/>";
 			s += "<span style='font-size:3px;color:transparent;opacity:0'>paragraph</span><br/>";
-			s += "<b>Open:</b> " + this.points[1].point.open.toLocaleString('en-US', {minimumFractionDigits: 2, maximumFractionDigits: 8}) + "<br/>";
-			s += "<b>High:</b> " + this.points[1].point.high.toLocaleString('en-US', {minimumFractionDigits: 2, maximumFractionDigits: 8}) + "<br/>";
-			s += "<b>Low:</b> " + this.points[1].point.low.toLocaleString('en-US', {minimumFractionDigits: 2, maximumFractionDigits: 8}) + "<br/>";
-			s += "<b>Close:</b> " + this.points[1].point.close.toLocaleString('en-US', {minimumFractionDigits: 2, maximumFractionDigits: 8}) + "<br/>";
+			s += "<b>Open:</b> " + formatCurrency(this.points[1].point.open) + "<br/>";
+			s += "<b>High:</b> " + formatCurrency(this.points[1].point.high) + "<br/>";
+			s += "<b>Low:</b> " + formatCurrency(this.points[1].point.low) + "<br/>";
+			s += "<b>Close:</b> " + formatCurrency(this.points[1].point.close) + "<br/>";
 			s += "<span style='font-size:3px;color:transparent;opacity:0'>paragraph</span><br/>";
-			s += "<b>Volume:</b> " + this.points[0].y.toLocaleString('en-US', {minimumFractionDigits: 2, maximumFractionDigits: 8}) + "<br/>";
+			s += "<b>Change:</b> " + Number((1 - (this.points[1].point.open / this.points[1].point.close)) * 100).toFixed(2) + "%<br>";
+			s += "<b>Amplitude:</b> " + Number((1 - (this.points[1].point.low / this.points[1].point.high)) * 100).toFixed(2) + "%<br>";
+			s += "<span style='font-size:3px;color:transparent;opacity:0'>paragraph</span><br/>";
+			s += "<b>Volume:</b> " + formatCurrency(this.points[0].y) + "<br/>";
 			return s;
 		},
 		split: false
@@ -150,6 +157,10 @@ function requestData(z) {
 			volume = [],
 			dataLength = data.length;
 		
+		chartPriceOpen = 0;
+		chartPriceClose = 0;
+		chartPriceMin = 0;
+		chartPriceMax = 0;
 		for (i = 0; i < dataLength; i++) {
 			if(data[i]['low'] > 0) {
 				ohlc.push([
@@ -164,6 +175,17 @@ function requestData(z) {
 					data[i]['time'] * 1000, // timestamp in milliseconds
 					data[i]['volumeto'] // volume
 				]);
+				
+				if(chartPriceOpen == 0) {
+					chartPriceOpen = data[i]['open'];
+				}
+				chartPriceClose = data[i]['close'];
+				if(chartPriceMin == 0 || chartPriceMin > data[i]['low']) {
+					chartPriceMin = data[i]['low'];
+				}
+				if(chartPriceMax == 0 || chartPriceMax < data[i]['high']) {
+					chartPriceMax = data[i]['high'];
+				}
 			}
 		}
 		chart.yAxis[0].update({type: scale});
@@ -171,17 +193,39 @@ function requestData(z) {
 		chart.series[1].setData(ohlc, false);
 		chart.series[0].setData(volume, false);
 		chart.redraw();
+		updateChartPrice();
 		updateSelect();
 	});
 }
 
+function updateChartPrice() {
+	var chartPriceDiff = chartPriceClose - chartPriceOpen;
+	var chartPriceDiffPercent = (1 - (chartPriceOpen / chartPriceClose)) * 100;
+	var sign = '';
+	if(chartPriceDiff > 0) {
+		sign = '+';
+		$("#chart-price-diff").removeClass();
+		$("#chart-price-diff").addClass("green");
+	}
+	else {
+		$("#chart-price-diff").removeClass();
+		$("#chart-price-diff").addClass("red");
+	}
+	var diff = sign + formatCurrency(chartPriceDiff) + ' (' + sign + chartPriceDiffPercent.toFixed(2) + '%)';
+	
+	$("#chart-price-close").text(formatCurrency(chartPriceClose));
+	$("#chart-price-diff").text(diff);
+	$("#chart-price-min").text(formatCurrency(chartPriceMin));
+	$("#chart-price-max").text(formatCurrency(chartPriceMax));
+}
+
 function updateSelect() {
-	// currencies
-	$("#select-currency option[value='" + currency + "']").prop('selected', true);
+	// basecurrency
 	$("#nav-basecurrency option[value='" + baseCurrency + "']").prop('selected', true);
+	// chart
+	$('input[type=radio][name=chart-zoom]').filter('[value=' + zoom + ']').prop('checked', true);
 	// settings
 	$('input[type=radio][name=radio-scale]').filter('[value=' + scale + ']').prop('checked', true);
-	$('input[type=radio][name=radio-zoom]').filter('[value=' + zoom + ']').prop('checked', true);
 	$('input[type=radio][name=radio-limit]').filter('[value=' + limit + ']').prop('checked', true);
 }
 
@@ -255,9 +299,9 @@ function loadOverview() {
 			tr.appendChild(document.createElement('td'));
 			tr.appendChild(document.createElement('td'));
 			tr.cells[0].appendChild(document.createTextNode(i));
-			tr.cells[1].appendChild(document.createTextNode(coin[baseCurrency]['PRICE'].toLocaleString('en-US', {minimumFractionDigits: 2, maximumFractionDigits: 8})));
-			tr.cells[2].appendChild(document.createTextNode(coin[baseCurrency]['MKTCAP'].toLocaleString('en-US', {minimumFractionDigits: 0, maximumFractionDigits: 0})));
-			tr.cells[3].appendChild(document.createTextNode(coin[baseCurrency]['TOTALVOLUME24HTO'].toLocaleString('en-US', {minimumFractionDigits: 0, maximumFractionDigits: 0})));
+			tr.cells[1].appendChild(document.createTextNode(formatCurrency(coin[baseCurrency]['PRICE'])));
+			tr.cells[2].appendChild(document.createTextNode(formatCurrency(coin[baseCurrency]['MKTCAP'])));
+			tr.cells[3].appendChild(document.createTextNode(formatCurrency(coin[baseCurrency]['TOTALVOLUME24HTO'])));
 			tbody.append(tr);
 		});
 		$("#tbody-overview tr").click(function() {
@@ -271,6 +315,30 @@ function toggleSettings() {
 	$("#settings-container").slideToggle();
 }
 
+// helper function
+function formatCurrency(x) {
+	if(x >= 10) { x = Number(x).toLocaleString('en-US', {minimumFractionDigits: 2, maximumFractionDigits: 2}); }
+	else if(x > 1) { x = Number(x).toLocaleString('en-US', {minimumFractionDigits: 3, maximumFractionDigits: 3}); }
+	else if(x == 1) { x = Number(x).toLocaleString('en-US', {minimumFractionDigits: 2, maximumFractionDigits: 2}); }
+	else if(x >= 0.1) { x = Number(x).toLocaleString('en-US', {minimumFractionDigits: 4, maximumFractionDigits: 4}); }
+	else if(x >= 0.01) { x = Number(x).toLocaleString('en-US', {minimumFractionDigits: 5, maximumFractionDigits: 5}); }
+	else if(x >= 0.001) { x = Number(x).toLocaleString('en-US', {minimumFractionDigits: 6, maximumFractionDigits: 6}); }
+	else if(x >= 0.0001) { x = Number(x).toLocaleString('en-US', {minimumFractionDigits: 7, maximumFractionDigits: 7}); }
+	else if(x >= 0.00001) { x = Number(x).toLocaleString('en-US', {minimumFractionDigits: 8, maximumFractionDigits: 8}); }
+	else if(x == 0) { x = Number(x).toLocaleString('en-US', {minimumFractionDigits: 2, maximumFractionDigits: 2}); }
+	
+	else if(x <= -10) { x = Number(x).toLocaleString('en-US', {minimumFractionDigits: 2, maximumFractionDigits: 2}); }
+	else if(x < -1) { x = Number(x).toLocaleString('en-US', {minimumFractionDigits: 3, maximumFractionDigits: 3}); }
+	else if(x == -1) { x = Number(x).toLocaleString('en-US', {minimumFractionDigits: 2, maximumFractionDigits: 2}); }
+	else if(x <= -0.1) { x = Number(x).toLocaleString('en-US', {minimumFractionDigits: 4, maximumFractionDigits: 4}); }
+	else if(x <= -0.01) { x = Number(x).toLocaleString('en-US', {minimumFractionDigits: 5, maximumFractionDigits: 5}); }
+	else if(x <= -0.001) { x = Number(x).toLocaleString('en-US', {minimumFractionDigits: 6, maximumFractionDigits: 6}); }
+	else if(x <= -0.0001) { x = Number(x).toLocaleString('en-US', {minimumFractionDigits: 7, maximumFractionDigits: 7}); }
+	else if(x <= -0.00001) { x = Number(x).toLocaleString('en-US', {minimumFractionDigits: 8, maximumFractionDigits: 8}); }
+
+	return x;
+}
+
 $(function() {
 	loadAllCurrencies();
 	
@@ -278,16 +346,16 @@ $(function() {
 	
 	chart = new Highcharts.stockChart('chart', options);
 
+	$('input[type=radio][name=chart-zoom]').change(function () {
+		changeZoom(this.value);
+	});
+	
 	$('#nav-settings').click(function(e) {
 		toggleSettings();
 	});
 	
 	$('input[type=radio][name=radio-scale]').change(function () {
 		changeScale(this.value);
-	});
-	
-	$('input[type=radio][name=radio-zoom]').change(function () {
-		changeZoom(this.value);
 	});
 	
 	$('input[type=radio][name=radio-limit]').change(function () {
